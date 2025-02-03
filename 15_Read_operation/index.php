6702394d6bd8fff7e17d310cc840ac8193b67346
satyajay01 <satyajaydibya0@gmail.com>
2025-02-03 16:47:16 +0530

    Select Operation

---
+++ 15_Read_operation/index.php
@@ -0,0 +1,18 @@
+<?php
+
+include("config/database.php");
+
+// Select Query 
+
+// $sql = "select * from users"; // for all tables data
+$sql = "select id, username, created_at from users"; // for selected table data 
+$sql = "select id, username, created_at from users where id = 1"; // for only one user data
+
+$result = $conn->query($sql);
+if($result->num_rows > 0){
+    while ($row = $result->fetch_assoc()) {
+        echo "<pre>";
+        print_r($row);
+    }
+}
+
